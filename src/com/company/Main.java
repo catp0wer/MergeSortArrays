fa3697248dd30cc4255f72a77dbc5ed3b99e8f86
laila <laila@fake.com>
2017-12-13 22:42:12 +0200

    adding insertion sorting to sort the merged array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,27 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] firstArray = {1, 5, 5};
         int[] secondArray = {2, 6, 7};
-        int[] thirdArray = {1, 4, 7, 8};
+        int[] thirdArray = {1, 4, 7, 8,2};
 
-        ArrayList<Integer> mergedArray = new ArrayList<>();
+        ArrayList<Integer> mergedArray = new ArrayList<Integer>();
 
         MergeArrays my_array = new MergeArrays();
-
 
         my_array.mergeArrays(firstArray, mergedArray);
         my_array.mergeArrays(secondArray, mergedArray);
         my_array.mergeArrays(thirdArray, mergedArray);
-        System.out.println(mergedArray);
+        System.out.println("merged array "+mergedArray);
+        //converting ArrayList to Array
+        Integer [] mergedSimpleArray = mergedArray.toArray(new Integer [mergedArray.size()]);
+
+        my_array.sortArray(mergedSimpleArray);
+
 
     }
 }
--- src/com/company/MergeArrays.java
+++ src/com/company/MergeArrays.java
@@ -12,5 +12,25 @@ public class MergeArrays {
 
         }
     }
-    
+
+    public void sortArray(Integer[] mergedSimpleArray) {
+        int temp;
+        for (int i = 1; i < mergedSimpleArray.length; i++) {
+            int j = i;
+            while (j > 0 && mergedSimpleArray[j - 1] > mergedSimpleArray[j]) {
+                //swapping
+                temp = mergedSimpleArray[j];
+                mergedSimpleArray[j] = mergedSimpleArray[j - 1];
+                mergedSimpleArray[j - 1] = temp;
+                //j-- is for comparing all right side
+                j--;
+            }
+
+        }printArray(mergedSimpleArray);
+    }
+
+    public static void printArray(Integer[] input_array){
+        for(int i=0; i < input_array.length; i++)
+            System.out.print(input_array[i] + " ");
+    }
 }
